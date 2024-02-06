c899817d9f3cdedc72048b3f17850f00db948cec
mason park <joung4342@gmail.com>
2024-02-06 20:57:51 +0900

    구간합

--- src/main/java/com/algorithm/Main.java
+++ src/main/java/com/algorithm/Main.java
@@ -1,30 +1,46 @@
 package com.algorithm;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tokenizer = new StringTokenizer(bf.readLine());
 
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int[] scoreArray = new int[N];
 
-        int max = 0;
-        int sum = 0;
-        for (int i = 0; i < N; i++) {
-            int score = sc.nextInt();
-            if (score > max) {
-                max = score;
+        int N = Integer.parseInt(tokenizer.nextToken());
+        int quiz = Integer.parseInt(tokenizer.nextToken());
+
+        int[][] table = new int[N + 1][N + 1];
+        for (int i = 1; i <= N; i++) {
+            tokenizer = new StringTokenizer(bf.readLine());
+
+            for (int j = 1; j <= N; j++) {
+                table[i][j] = Integer.parseInt(tokenizer.nextToken());
             }
-            sum += score;
-            scoreArray[i] = score;
         }
-        System.out.println(sum * 100.0 / max / N);
+
+        int[][] sumTable = new int[N + 1][N + 1];
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                sumTable[i][j] = (sumTable[i][j - 1] + sumTable[i - 1][j] - sumTable[i - 1][j - 1] + table[i][j]);
+            }
+        }
+
+
+
+
+        for (int q = 0; q < quiz; q++) {
+            tokenizer = new StringTokenizer(bf.readLine());
+            int x1 = Integer.parseInt(tokenizer.nextToken());
+            int y1 = Integer.parseInt(tokenizer.nextToken());
+            int x2 = Integer.parseInt(tokenizer.nextToken());
+            int y2 = Integer.parseInt(tokenizer.nextToken());
+            System.out.println(sumTable[x2][y2] - sumTable[x1 - 1][y2] - sumTable[x2][y1 - 1] + sumTable[x1 - 1][y1 - 1]);
+        }
 
     }
 }
-// 80
-
-// 40/80*100
-// 60/80*100
-// 80/80*100
