55911114e747de1b243827ed62707fe1a58fedc0
mason park <joung4342@gmail.com>
2024-02-15 18:09:47 +0900

    나머지 합

--- src/main/java/com/algorithm/Main.java
+++ src/main/java/com/algorithm/Main.java
@@ -1,46 +1,35 @@
 package com.algorithm;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer tokenizer = new StringTokenizer(bf.readLine());
+    public static void main(String[] args)  {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        long res = 0L;
 
+        long[] sumArr = new long[N];
+        sumArr[0] = sc.nextInt();
+        for (int i = 1; i < N; i++) {
+            sumArr[i] = sumArr[i - 1] + sc.nextInt();
+        }
 
-        int N = Integer.parseInt(tokenizer.nextToken());
-        int quiz = Integer.parseInt(tokenizer.nextToken());
+        long[] remainCntArr = new long[M];
+        for (int i = 0; i < N; i++) {
+            int remain = (int) (sumArr[i] % M);
 
-        int[][] table = new int[N + 1][N + 1];
-        for (int i = 1; i <= N; i++) {
-            tokenizer = new StringTokenizer(bf.readLine());
+            if (remain == 0) res++;
 
-            for (int j = 1; j <= N; j++) {
-                table[i][j] = Integer.parseInt(tokenizer.nextToken());
+            remainCntArr[remain]++;
+        }
+
+        for (int i = 0; i < M; i++) {
+            if (remainCntArr[i] > 1) {
+                res += (remainCntArr[i] * (remainCntArr[i] - 1)) / 2;
             }
         }
-
-        int[][] sumTable = new int[N + 1][N + 1];
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= N; j++) {
-                sumTable[i][j] = (sumTable[i][j - 1] + sumTable[i - 1][j] - sumTable[i - 1][j - 1] + table[i][j]);
-            }
-        }
-
-
-
-
-        for (int q = 0; q < quiz; q++) {
-            tokenizer = new StringTokenizer(bf.readLine());
-            int x1 = Integer.parseInt(tokenizer.nextToken());
-            int y1 = Integer.parseInt(tokenizer.nextToken());
-            int x2 = Integer.parseInt(tokenizer.nextToken());
-            int y2 = Integer.parseInt(tokenizer.nextToken());
-            System.out.println(sumTable[x2][y2] - sumTable[x1 - 1][y2] - sumTable[x2][y1 - 1] + sumTable[x1 - 1][y1 - 1]);
-        }
+        System.out.println(res);
 
     }
 }
