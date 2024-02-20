e73c6aac6c72fca6e99345c4ec6c82a195e3bc11
mason park <joung4342@gmail.com>
2024-02-20 20:42:42 +0900

    투포인터4_2

--- src/main/java/com/algorithm/Main.java
+++ src/main/java/com/algorithm/Main.java
@@ -2,33 +2,43 @@ package com.algorithm;
 
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class Main {
-    public static char[] DNA = {'A', 'C', 'G', 'T'};
+    public static int[] chkCntArr = new int[4];
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int res = 0;
 
         int dnaCharLen = sc.nextInt();
-        char[] passWordStr = new char[sc.nextInt()];
+        int passwordLen = sc.nextInt();
         String charStr = null;
-
-        int[] chkCntArr = new int[DNA.length];
         do {
             charStr = sc.nextLine();
         } while (charStr.length() != dnaCharLen);
 
-        char[] dnacharArr = charStr.toCharArray();
-        int chkResCnt = 0;
         for (int i = 0; i < chkCntArr.length; i++) {
             chkCntArr[i] = sc.nextInt();
-            if (chkCntArr[i] == 0) {
-                chkResCnt++;
-            }
         }
 
+        int[] currArr = new int[4];
+        for (int i = 0; i < passwordLen; i++) {
+            if("A".equals(charStr.charAt(i))) currArr[i]++;
+            if("C".equals(charStr.charAt(i))) currArr[i]++;
+            if("G".equals(charStr.charAt(i))) currArr[i]++;
+            if("T".equals(charStr.charAt(i))) currArr[i]++;
+        }
+        if (isRightPassword(currArr)) {
+            res++;
+        }
 
-        char[] currArr = new char[DNA.length];
+        for (int i = passwordLen; i < dnaCharLen; i++) {
 
+        }
+    }
+
+    private static boolean isRightPassword(int[] currArr) {
+        return IntStream.range(0, chkCntArr.length)
+                .allMatch(i -> currArr[i] >= chkCntArr[i]);
     }
 }
