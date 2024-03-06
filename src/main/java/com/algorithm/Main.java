86a3f6709b6e572710c5751f15a80057b7ae890d
mason park <joung4342@gmail.com>
2024-03-06 20:29:51 +0900

    최소값

--- src/main/java/com/algorithm/Main.java
+++ src/main/java/com/algorithm/Main.java
@@ -1,74 +1,78 @@
 package com.algorithm;
 
+
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-import java.util.stream.IntStream;
+import java.io.OutputStreamWriter;
 
 public class Main {
-
-    static int strLen;
-    static int passwordLen;
-    static char[] str;
-    static int[] checkArr = new int[4];
-    static int[] myArr = new int[4];
-    static int checkCnt = 0; // {‘A’, ‘C’, ‘G’, ‘T’} 중 최소 개수를 일치한 문자 개수 (0~4)
-    static int answer = 0; // 만들 수 있는 비밀번호 개수
-
     public static void main(String[] args) throws IOException {
-
-        // 입력
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         StringTokenizer st = new StringTokenizer(br.readLine());
-        strLen = Integer.parseInt(st.nextToken());
-        passwordLen = Integer.parseInt(st.nextToken());
-        str = br.readLine().toCharArray(); // DNA 문자열
+        int N = Integer.parseInt(st.nextToken());
+        int L = Integer.parseInt(st.nextToken());
+
         st = new StringTokenizer(br.readLine());
 
-        for (int i = 0; i < 4; i++) {
-            checkArr[i] = Integer.parseInt(st.nextToken());
+        // 값을 보관 및 정렬 해 줄 deque
+        Deque<Node> mydeque = new LinkedList<>();
+        for (int i = 0; i < N; i++) {
+            int now = Integer.parseInt(st.nextToken());
+
+            // 덱이 비거나, 덱 마지막에 현재 값(now)보다 큰 값 지우기
+            while (!mydeque.isEmpty()
+                    && mydeque.getLast().getValue() > now) {
+
+                mydeque.removeLast();
+            }
+            // 덱 마지막에 값 삽입
+            mydeque.addLast(new Node(i, now));
+
+            // 덱의 첫번째 인덱스에서 범위를 벗어난 값 지우기
+            if(mydeque.getFirst().getIndex() <= i - L) {
+                mydeque.removeFirst();
+            }
+
+            // 최소값 출력
+            bw.write(mydeque.getFirst().getValue() + " ");
         }
-
-        for (int i = 0; i < passwordLen; i++) { // 첫 부분문자열 셋팅 (0~p_len-1) 까지
-            if (str[i]=='A') myArr[0]++;
-            if (str[i]=='C') myArr[1]++;
-            if (str[i]=='G') myArr[2]++;
-            if (str[i]=='T') myArr[3]++;
-        }
-
-        if (isRightPassword())// {‘A’, ‘C’, ‘G’, ‘T’} 4개의 문자가 모두 최소개수를 만족했다면
-            answer++; // 만들 수 있는 비밀번호 개수 증가
-
-        int i = -1;
-        /**
-         * 부분문자열 만들기 => 이전 부분문자열의 첫 문자는 제외하고, 끝에서 1문자를 더 추가한다.
-         */
-        for (int j = passwordLen; j < strLen; j++) { // 부분문자열의 끝을 나타내는 위치
-            i = j - passwordLen; // 이전 부분문자열의 시작을 나타내는 위치
-
-            // 이전 부분문자열의 시작 문자 제외
-            if (str[i]=='A') myArr[0]--;
-            if (str[i]=='C') myArr[1]--;
-            if (str[i]=='G') myArr[2]--;
-            if (str[i]=='T') myArr[3]--;
-
-            // 이전 부분문자열의 끝에서 1문자 추가
-            if (str[j]=='A') myArr[0]++;
-            if (str[j]=='C') myArr[1]++;
-            if (str[j]=='G') myArr[2]++;
-            if (str[j]=='T') myArr[3]++;
-
-            if (isRightPassword())// {‘A’, ‘C’, ‘G’, ‘T’} 4개의 문자가 모두 최소개수를 만족했다면
-                answer++; // 만들 수 있는 비밀번호 개수 증가
-        }
-
-        System.out.println(answer);
-
+        bw.flush();
+        bw.close();
     }
 
-    private static boolean isRightPassword() {
-        return IntStream.range(0, checkArr.length)
-                .allMatch(i -> myArr[i] >= checkArr[i]);
+    static class Node {
+        private int index;
+        private int value;
+
+        public Node(int index, int value) {
+            this.index = index;
+            this.value = value;
+        }
+
+        public int getIndex() {
+            return index;
+        }
+
+        public void setIndex(int index) {
+            this.index = index;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public void setValue(int value) {
+            this.value = value;
+        }
     }
 }
+
