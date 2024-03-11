2b6162840c3c59643bb49922b96746dc1b506064
mason park <joung4342@gmail.com>
2024-03-11 20:13:45 +0900

    Stack

--- src/main/java/com/algorithm/Main.java
+++ src/main/java/com/algorithm/Main.java
@@ -1,78 +1,39 @@
 package com.algorithm;
 
 
-
-import java.util.Deque;
-import java.util.LinkedList;
-import java.util.StringTokenizer;
-
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.util.Scanner;
+import java.util.Stack;
+import java.util.stream.IntStream;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int maxNum = sc.nextInt();
+        int[] numArr = new int[maxNum];
+        IntStream.range(0, maxNum)
+                .forEach(i -> numArr[i] = sc.nextInt());
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int L = Integer.parseInt(st.nextToken());
-
-        st = new StringTokenizer(br.readLine());
-
-        // 값을 보관 및 정렬 해 줄 deque
-        Deque<Node> mydeque = new LinkedList<>();
-        for (int i = 0; i < N; i++) {
-            int now = Integer.parseInt(st.nextToken());
-
-            // 덱이 비거나, 덱 마지막에 현재 값(now)보다 큰 값 지우기
-            while (!mydeque.isEmpty()
-                    && mydeque.getLast().getValue() > now) {
-
-                mydeque.removeLast();
+        Stack<Integer> stack = new Stack<>();
+        StringBuilder printer = new StringBuilder();
+        int standard = 1;
+        boolean result = true;
+        for (int curNum : numArr) {
+            if (curNum >= standard) {
+                while (curNum >= standard) {
+                    stack.push(standard++);
+                    printer.append("+\n");
+                }
             }
-            // 덱 마지막에 값 삽입
-            mydeque.addLast(new Node(i, now));
-
-            // 덱의 첫번째 인덱스에서 범위를 벗어난 값 지우기
-            if(mydeque.getFirst().getIndex() <= i - L) {
-                mydeque.removeFirst();
+            if (stack.isEmpty() || stack.peek() != curNum) {
+                System.out.println("NO");
+                result = false;
+                break;
             }
-
-            // 최소값 출력
-            bw.write(mydeque.getFirst().getValue() + " ");
+            stack.pop();
+            printer.append("-\n");
         }
-        bw.flush();
-        bw.close();
-    }
-
-    static class Node {
-        private int index;
-        private int value;
-
-        public Node(int index, int value) {
-            this.index = index;
-            this.value = value;
-        }
-
-        public int getIndex() {
-            return index;
-        }
-
-        public void setIndex(int index) {
-            this.index = index;
-        }
-
-        public int getValue() {
-            return value;
-        }
-
-        public void setValue(int value) {
-            this.value = value;
-        }
+        if (result) System.out.println(printer.toString());
     }
 }
+
 
