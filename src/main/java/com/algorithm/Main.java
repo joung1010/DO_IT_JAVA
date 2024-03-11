6cbc9040ed688bf4ea301ed247c643d09542c6ef
mason park <joung4342@gmail.com>
2024-03-11 20:46:29 +0900

    Stack3

--- src/main/java/com/algorithm/Main.java
+++ src/main/java/com/algorithm/Main.java
@@ -3,15 +3,20 @@ package com.algorithm;
 
 import java.io.*;
 import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         int[] numArr = new int[n];
+
+        // 한 줄에 모든 수를 입력받아서 처리
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            numArr[i] = Integer.parseInt(br.readLine());
+            numArr[i] = Integer.parseInt(st.nextToken());
         }
+
         int[] resArr = new int[n];
         Stack<Integer> stack = new Stack<>();
         stack.push(0);
@@ -29,13 +34,13 @@ public class Main {
 
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         for (int i = 0; i < n; i++) {
-            bw.append(resArr[i] + " ");
+            bw.write(resArr[i] + " ");  // 공백으로 구분하여 출력
         }
         bw.flush();
-
         bw.close();
         br.close();
     }
 }
+
 
 
