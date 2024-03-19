32fe32473457da1e3f0cf5656d0056d67b8f7835
mason park <joung4342@gmail.com>
2024-03-19 19:21:20 +0900

    Queue1

--- src/main/java/com/algorithm/Main.java
+++ src/main/java/com/algorithm/Main.java
@@ -1,44 +1,23 @@
 package com.algorithm;
 
 
-import java.io.*;
-import java.util.Stack;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.util.stream.IntStream;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int[] numArr = new int[n];
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
 
-        // 한 줄에 모든 수를 입력받아서 처리
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            numArr[i] = Integer.parseInt(st.nextToken());
+        Deque<Integer> queue = new LinkedList<>();
+        IntStream.rangeClosed(1, N)
+                .forEach(queue::add);
+
+        while (queue.size() > 1) {
+            queue.poll();
+            queue.add(queue.poll());
         }
-
-        int[] resArr = new int[n];
-        Stack<Integer> stack = new Stack<>();
-        stack.push(0);
-
-        for (int i = 1; i < n; i++) {
-            while (!stack.isEmpty() && numArr[stack.peek()] < numArr[i]) {
-                resArr[stack.pop()] = numArr[i];
-            }
-            stack.push(i);
-        }
-
-        while (!stack.isEmpty()) {
-            resArr[stack.pop()] = -1;
-        }
-
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        for (int i = 0; i < n; i++) {
-            bw.write(resArr[i] + " ");  // 공백으로 구분하여 출력
-        }
-        bw.flush();
-        bw.close();
-        br.close();
+        System.out.println(queue.poll());
     }
 }
 
