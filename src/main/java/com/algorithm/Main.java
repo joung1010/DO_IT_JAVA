c5b3d5504cbeba6462f09f12d4acdc28c2fb9020
mason park <joung4342@gmail.com>
2024-03-26 19:01:24 +0900

    선택정렬

--- src/main/java/com/algorithm/Main.java
+++ src/main/java/com/algorithm/Main.java
@@ -1,44 +1,23 @@
 package com.algorithm;
 
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(bf.readLine());
+    public static void main(String[] args)  {
+        Scanner sc = new Scanner(System.in);
+        String number = sc.next();
 
-        Node[] nodeArray = new Node[N];
-        for (int i = 0; i < N; i++) {
-            nodeArray[i] = new Node(Integer.parseInt(bf.readLine()), i);
-        }
-
-        Arrays.sort(nodeArray);
-        int result = 0;
-        for (int i = 0; i < N; i++) {
-            if (result < nodeArray[i].index - i) {
-                result = nodeArray[i].index - i;
-            }
-        }
-        System.out.println(result + 1);
-    }
-
-    static class Node implements Comparable<Node> {
-        int value;
-        int index;
-
-        public Node(int value, int index) {
-            this.value = value;
-            this.index = index;
-        }
-
-        @Override
-        public int compareTo(Node o) {
-            return this.value - o.value;
-        }
+        String sortedNum = Arrays.asList(String.valueOf(number).split(""))
+                .stream()
+                .map(Integer::parseInt)
+                .sorted(Comparator.reverseOrder())
+                .map(String::valueOf)
+                .collect(Collectors.joining());
+        System.out.println(sortedNum);
     }
 }
 
