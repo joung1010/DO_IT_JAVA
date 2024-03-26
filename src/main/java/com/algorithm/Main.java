c6347ba9b320d5a9564725882d434f0a62f3d55b
mason park <joung4342@gmail.com>
2024-03-26 19:55:08 +0900

    삽입 정렬

--- src/main/java/com/algorithm/Main.java
+++ src/main/java/com/algorithm/Main.java
@@ -1,23 +1,31 @@
 package com.algorithm;
 
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.Scanner;
-import java.util.stream.Collectors;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args)  {
-        Scanner sc = new Scanner(System.in);
-        String number = sc.next();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
 
-        String sortedNum = Arrays.asList(String.valueOf(number).split(""))
-                .stream()
-                .map(Integer::parseInt)
-                .sorted(Comparator.reverseOrder())
-                .map(String::valueOf)
-                .collect(Collectors.joining());
-        System.out.println(sortedNum);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        List<Integer> waitTimeList = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            waitTimeList.add(Integer.parseInt(st.nextToken()));
+        }
+
+        Collections.sort(waitTimeList);
+        int totalSum = 0;
+        int cumulativeSum = 0;
+        for (Integer num : waitTimeList) {
+            cumulativeSum += num;
+            totalSum += cumulativeSum;
+        }
+
+        System.out.println(totalSum);
     }
 }
 
