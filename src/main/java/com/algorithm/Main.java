279e04bf77d10d88abc0ba88f45f35c0a9ae989d
mason park <joung4342@gmail.com>
2024-04-08 20:17:16 +0900

    병합정렬

--- src/main/java/com/algorithm/Main.java
+++ src/main/java/com/algorithm/Main.java
@@ -1,33 +1,64 @@
 package com.algorithm;
 
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.*;
+import java.io.*;
 
 public class Main {
+    public static int[] intArr;
+    public static int[] tmpArr;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int totalCount = Integer.parseInt(st.nextToken());
-        int targetIdx = Integer.parseInt(st.nextToken());
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        st = new StringTokenizer(br.readLine());
-        List<Integer> integerList = new ArrayList<>();
-        for (int i = 0; i < totalCount; i++) {
-            integerList.add(Integer.parseInt(st.nextToken()));
+        int totalNum = Integer.parseInt(br.readLine());
+        intArr = new int[totalNum];
+        tmpArr = new int[totalNum];
+
+        for (int i = 0; i < totalNum; i++) {
+            intArr[i] = Integer.parseInt(br.readLine());
         }
 
-        int targetNum = integerList.stream()
-                .sorted()
-                .mapToInt(Integer::intValue)
-                .skip(targetIdx - 1)
-                .findFirst()
-                .orElse(0);
+        mergeSort(0, totalNum - 1);
+        for (int i = 0; i < totalNum; i++) {
+            bw.write(intArr[i] + "\n");
+        }
 
-        System.out.println(targetNum);
+        bw.flush();
+        bw.close();
+    }
+
+    public static void mergeSort(int start, int end) {
+        if (start >= end) {
+            return;
+        }
+
+        int mid = (start + end) / 2;
+
+        mergeSort(start, mid);
+        mergeSort(mid + 1, end);
+
+        for (int i = start; i <= end; i++) {
+            tmpArr[i] = intArr[i];
+        }
+
+        int grpIdx1 = start;
+        int grpIdx2 = mid + 1;
+        int target = start;
+
+        while (grpIdx1 <= mid && grpIdx2 <= end) {
+            if (tmpArr[grpIdx1] <= tmpArr[grpIdx2]) {
+                intArr[target++] = tmpArr[grpIdx1++];
+            } else {
+                intArr[target++] = tmpArr[grpIdx2++];
+            }
+        }
+
+        while (grpIdx1 <= mid) {
+            intArr[target++] = tmpArr[grpIdx1++];
+        }
+
+        while (grpIdx2 <= end) {
+            intArr[target++] = tmpArr[grpIdx2++];
+        }
     }
 }
-
-
