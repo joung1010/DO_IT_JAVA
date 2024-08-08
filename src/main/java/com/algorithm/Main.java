6f183ff25b53dd23dc092bb104f8ce59e7abcddb
mason park <joung4342@gmail.com>
2024-08-08 20:12:28 +0900

    feat(quiz): 두수의 조합해서 큰값을 기준으로 정렬(성공 했지만 시간초과건 존재)

--- src/main/java/com/algorithm/Main.java
+++ src/main/java/com/algorithm/Main.java
@@ -1,28 +1,57 @@
 package com.algorithm;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class Main {
+
+    /*
+    * 6
+610 , 106
+62, 26
+
+10
+102, 210 102 > 210
+maxIdx = 3
+
+
+temp = 10
+curr =  2
+6 2 10
+    *
+    * */
     public static String solution(int[] numbers) {
 
-        int length = numbers.length;
+        final int length = numbers.length;
+        String[] numbersStrArr = new String[length];
         for (int i = 0; i < length; i++) {
-            int minIdx = i;
+            numbersStrArr[i] = String.valueOf(numbers[i]);
+        }
+
+        for (int i = 0; i < numbersStrArr.length; i++) {
+            int maxIdx = i;
             for (int j = i + 1; j < length; j++) {
-                if (numbers[j] % 10 > numbers[minIdx] % 10) {
-                    minIdx = j;
+                String strNum1 = numbersStrArr[j] + numbersStrArr[maxIdx];
+                String strNum2 = numbersStrArr[maxIdx] + numbersStrArr[j];
+                if (strNum1.compareTo(strNum2) > 0) {
+                    maxIdx = j;
                 }
             }
-            int tmp = numbers[minIdx];
-            numbers[minIdx] = numbers[i];
-            numbers[i] = tmp;
-
+            String tmp = numbersStrArr[maxIdx];
+            numbersStrArr[maxIdx] = numbersStrArr[i];
+            numbersStrArr[i] = tmp;
         }
+
+        if (numbersStrArr[0].equals("0")) {
+            return "0";
+        }
+
         StringBuilder result = new StringBuilder();
-        for (int number : numbers) {
-            result.append(number);
+        for (String strNum : numbersStrArr) {
+            result.append(strNum);
         }
         return result.toString();
     }
